f881d9846fbc87ee7882fec0b44e82ed7608cd9d
PEDRO HENRIQUE TOMAZ ROMAO <12537141695>
2022-10-31 09:51:33 -0300

    Ç

--- predictions.py
+++ predictions.py
@@ -1,5 +1,21 @@
 import torch, numpy as np
 
+class Net(torch.nn.Module):
+  def __init__(self, input_size, hidden_size):
+    super(Net, self).__init__()
+    self.input_size = input_size
+    self.hidden_size = hidden_size
+    self.fc1 = torch.nn.Linear(self.input_size, self.hidden_size) #full connected
+    self.relu = torch.nn.ReLU() #(0, infinito)
+    self.fc2 = torch.nn.Linear(self.hidden_size, 1)
+    self.sigmoid = torch.nn.Sigmoid() #(0, 1)
+  def forward(self, x):
+    hidden = self.fc1(x)
+    relu = self.relu(hidden)
+    output = self.fc2(relu)
+    output = self.sigmoid(output)
+    return output
+
 modelo = Net()
 
 modelo.load_state_dict(torch.load('./modelo/iris.dat'))
