02b67b546cbb0624bf0f2fd73076f59018b0525e
PEDRO HENRIQUE TOMAZ ROMAO <12537141695>
2022-10-31 09:21:15 -0300

    New route

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Flask, jsonify, render_template, request
 import os
 
@@ -8,6 +9,12 @@ app = Flask(__name__, template_folder='templates')
 def index():
     return render_template("index.html")
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        return jsonify(request.form)
+    else:
+        return jsonify({"Mensagem":"Utilize o formulario"})
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
